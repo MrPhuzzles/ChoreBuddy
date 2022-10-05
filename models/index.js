832b77a2ac936970cf6c8217457168ce01f89bec
raposamillar <lisa.raposamillar@gmail.com>
2022-10-04 21:06:35 -0400

    create directories

---
+++ models/index.js
@@ -0,0 +1,8 @@
+// import all models 
+const Post = require('./Post');
+const User = require('./User');
+const Rating = require('./Rating');
+
+// create associations
+
+module.exports = { User, Post, Rating};

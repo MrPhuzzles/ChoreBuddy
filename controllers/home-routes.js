3f305e29cb2c690f838d04758157b8640f03cda6
Tmeng <tonymengt@gmail.com>
2022-10-10 11:48:47 -0400

    updated the home-routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,27 +4,22 @@ const {User, Post, Reviews, Rel} = require('../models');
 router.get('/', (req, res) => {
 
     Post.findAll({
-        attributes: ['id','title', 'content', 'created_at'],
-        order:[
-          ['created_at', 'DESC']
-        ],
+        attributes: ['title', 'content', 'created_at'],
         include: [{
-            model:User,
-            attributes: ['username']
+            model: User,
+            attributes: ['username'],
         }]
     })
-    .then(dbPostData =>  {
-        const post = dbPostData.map((post) => post.get({plain: true}));
-        res.render("homepage", {
-            post
-        })
+    .then(dbPostData => {
+        const post = dbPostData.map((post) => post.get({plain:true}));
+
+        res.render("homepage", {post})
     })
     .catch(err => {
         console.log(err);
         res.status(500).json(err);
     })
 })
-
 
 
 module.exports = router;
